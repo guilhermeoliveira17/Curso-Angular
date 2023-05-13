35a7ad3c42dd0d24a2abb278714b38c50335fd95
Guilherme <guilhermeoliveira13579112@gmail.com>
2023-05-13 13:48:56 -0300

    Utilizando api para trazer os dados dos cursos

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,16 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
+import { take, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'React-native', category: 'mobile'}
-    ];
+  private readonly API = '/assets/courses.json';
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      take(1),
+      tap(courses => console.log(courses))
+    );
   }
 
   constructor(private httpClient: HttpClient) { }
